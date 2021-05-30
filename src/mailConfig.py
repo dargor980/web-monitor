0451f870f1310920e183f72fab2640fcee20d25e
dargor980 <german.contrerasa@utem.cl>
2021-05-30 15:48:55 -0400

    ADD: parseData

--- src/mailConfig.py
+++ src/mailConfig.py
@@ -4,31 +4,28 @@ import time
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+def parseData(data):
+    aux=""
+    for i in data:
+        aux += " ".join(i)
+    return aux
+
 
 def sendEmail(content):
-    sites = " ".join(content)
+    sites = parseData(content)
     msg = MIMEMultipart()
-
-    message = "Se ha detectado una caída en el/los sitio(s)." + sites + "\nFecha y hora de la incidencia: " + time.strftime("%d/%m/%y") + " " + time.strftime("%X")
-
+    print("lista: ", sites)
+    message = "Se ha detectado una caída en el/los sitio(s):\n" + sites + "\nFecha y hora de la incidencia: " + time.strftime("%d/%m/%y") + " " + time.strftime("%X")
     password = "vllbckfxutrtnbba"
-
     msg['From'] = "german.contrerasa@utem.cl"
     msg['To'] = "cafesitomygod@gmail.com"
     msg['Subject'] = "Sitio(s) Caído(s)"
-
     msg.attach(MIMEText(message, 'plain'))
-
     try:
-
         server = smtplib.SMTP('smtp.gmail.com: 587')
-
         server.starttls()
-
         server.login(msg['From'], password)
-
         server.sendmail(msg['From'], msg['To'], msg.as_string())
-
         server.quit()
     except socket.error as e:
         print("No se pudo realizar la conexión")
--- src/main.py
+++ src/main.py
@@ -8,34 +8,27 @@ import mailConfig
 webAdresses = []
 with open('src/sites.json') as file:
     sites = json.load(file)
-
     for websites in sites['sites']:
         webAdresses.append(websites['url'])
 
 downWebSites = []
 
-mailConfig.sendEmail(downWebSites)
 while True:
-
     for req in webAdresses:
         try:
-
             r = requests.get(req)
             if r.status_code == requests.codes.ok:
                 print("OK")
             else:
-                downWebSites.append(req)
+                downWebSites.append([req," - Error: " + str(r.status_code) + "\n"])
         except requests.exceptions.RequestException:
             print("URL NO EXISTENTE")
-            downWebSites.append(req)
-    
-   
-
-
+            downWebSites.append([req, " - Error: DNS_PROBE_FINISHED_NXDOMAIN\n"])
     if downWebSites:
         print("SITIOS CAIDOS")
+        mailConfig.sendEmail(downWebSites)
+        downWebSites = []
     else:
         print("NO SE CAYERON SITIOS EN ESTA ITERACION")
-        
     time.sleep(300)
 
